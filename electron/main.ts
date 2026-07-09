2a903228ceb2edf05393e0fd2aabe82d1027de93
dev <dev@multiround.local>
2026-07-10 01:25:39 +0800

    Fix: ESM module config, __dirname polyfill, rebuild electron dist

--- electron/main.ts
+++ electron/main.ts
@@ -2,6 +2,10 @@
 
 import { app, BrowserWindow, ipcMain, Menu, MenuItemConstructorOptions, dialog, shell } from 'electron';
 import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 import fs from 'node:fs';
 import Store from 'electron-store';
